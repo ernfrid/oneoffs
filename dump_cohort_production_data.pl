33983862b84e80a3299a9f4cd78fdf7c0df1a0a8
Dave Larson <dlarson@genome.wustl.edu>
2015-12-10 16:22:23 -0600

    add gc bias summary stats and flagstat duplication rate

--- dump_cohort_production_data.pl
+++ dump_cohort_production_data.pl
@@ -8,7 +8,7 @@ use Genome;
 my @anp_ids = @ARGV;
 my @anps = Genome::Config::AnalysisProject->get(id => \@anp_ids);
 
-print join("\t", qw(Sample Analysis_Project Model Build Primary_flowcell Primary_date Primary_machine All_flowcells nLanes nFlowcells Freemix Median_Insert_Size MAD_Insert_Size Mean_Insert_Size SD_Insert_Size )), "\n";
+print join("\t", qw(Sample Analysis_Project Model Build Primary_flowcell Primary_date Primary_machine All_flowcells nLanes nFlowcells Freemix Median_Insert_Size MAD_Insert_Size Mean_Insert_Size SD_Insert_Size GC_Dropout AT_Dropout Flagstat_Duplication_Rate)), "\n";
 
 for my $anp (@anps) {
     my @models = Genome::Model->get(analysis_project => $anp, 'config_profile_item.tag_names' => 'production qc') or die "Unable to get a models for ", $anp->name, "\n";
@@ -24,7 +24,7 @@ for my $anp (@anps) {
         my $primary_machine = key_for_flowcell($machines, $primary);
         my $fm = freemix($model);
         my @instrument_data = $model->instrument_data;
-        print join("\t", $model->subject->name, $anp->name, $model->id, $model->last_succeeded_build->id, $primary, $primary_date, $primary_machine, join(",", keys %$flowcells), scalar(@instrument_data), scalar(keys %$flowcells), $fm, insert_size_metrics($model)), "\n";
+        print join("\t", $model->subject->name, $anp->name, $model->id, $model->last_succeeded_build->id, $primary, $primary_date, $primary_machine, join(",", keys %$flowcells), scalar(@instrument_data), scalar(keys %$flowcells), $fm, insert_size_metrics($model), gc_bias_metrics($model), flagstat_duplication_rate($model)), "\n";
     }
 }
 
@@ -43,11 +43,23 @@ sub qc_metrics_hash {
     }
 }
 
+sub gc_bias_metrics {
+    my ($model) = @_;
+    my $metrics = qc_metrics_hash($model) or return undef;
+    return @{$metrics}{qw( GC_DROPOUT AT_DROPOUT )},
+}
+    
 
 sub insert_size_metrics {
     my ($model) = @_;
     my $metrics = qc_metrics_hash($model) or return undef;
     return @{$metrics->{'FR'}}{qw( MEDIAN_INSERT_SIZE MEDIAN_ABSOLUTE_DEVIATION MEAN_INSERT_SIZE STANDARD_DEVIATION ) };
+}
+
+sub flagstat_duplication_rate {
+    my ($model) = @_;
+    my $metrics = qc_metrics_hash($model) or return undef;
+    return sprintf "%0.02f", $metrics->{'reads_marked_duplicates'} / $metrics->{'reads_mapped'} * 100;
 }
 
 sub freemix {
