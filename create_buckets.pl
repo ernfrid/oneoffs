0de71d86f8cf33b6078cb6e7f246de17cf7d0a99
Dave Larson <dlarson@genome.wustl.edu>
2016-01-06 21:39:37 -0600

    script to create buckets for grch38 plus alts for GMS

---
+++ create_buckets.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env genome-perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use Genome;
+use Genome::Model::ReferenceSequence::Command::CreateBuckets;
+
+my $build = Genome::Model::Build->get(q{8741b5363c634ee19fad676157a132de});
+
+my $object = Genome::Model::ReferenceSequence::Command::CreateBuckets->execute(
+    reference_sequence_build => $build,
+    requestor => $build,
+);
+
+print Dumper $object->output_result;
