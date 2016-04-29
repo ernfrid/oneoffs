c324ba9b7f83fd93c7ffe1b36ff573dcc3108fbe
Dave Larson <dlarson@genome.wustl.edu>
2016-04-29 14:01:18 -0500

    seems to work

--- benchmarkthis.sh
+++ benchmarkthis.sh
@@ -2,7 +2,16 @@
 
 args=("$@")
 times=${args[0]} #number of times to run the command
-jobname=${args[1]} #what to call these jobs
-hostname=${args[2]} #what host to use
+queue=${args[1]}
+jobname=${args[2]} #what to call these jobs
+hostname=${args[3]} #what host to use
+cmd=${args[*]:4}
 
-echo "times: $times, jobname: $jobname, hostname: $hostname, command: ${args[*]:3}"
+#bsub initial job
+bsub -N -u dlarson@genome.wustl.edu -q $queue -m "$hostname" -J $jobname.1 $cmd
+
+for ((i=2;i<=times;i++)); do
+    prev=$((i - 1))
+    bsub -N -u dlarson@genome.wustl.edu -q $queue -m "$hostname" -J $jobname.$i -w "done($jobname.$prev)" $cmd
+done
+exit 0
