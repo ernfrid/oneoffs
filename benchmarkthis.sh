be6ce90cc51c956e7a26bc74ab750ecf82081c49
Dave Larson <dlarson@genome.wustl.edu>
2016-05-10 13:53:00 -0500

    add processors to arguments

--- benchmarkthis.sh
+++ benchmarkthis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 args=("$@")
 times=${args[0]} #number of times to run the command
@@ -7,11 +7,12 @@ jobname=${args[2]} #what to call these jobs
 hostname=${args[3]} #what host to use
 cmd=${args[*]:4}
 
+processors=`bhosts -w $hostname | sed 's/\s\+/	/g' | grep -v MAX | cut -f4`
 #bsub initial job
-bsub -N -u dlarson@genome.wustl.edu -q $queue -m "$hostname" -J $jobname.1 $cmd
+bsub -N -u dlarson@genome.wustl.edu -n $processors -q $queue -m "$hostname" -J $jobname.1 $cmd
 
 for ((i=2;i<=times;i++)); do
     prev=$((i - 1))
-    bsub -N -u dlarson@genome.wustl.edu -q $queue -m "$hostname" -J $jobname.$i -w "done($jobname.$prev)" $cmd
+    bsub -N -u dlarson@genome.wustl.edu -n $processors -q $queue -m "$hostname" -J $jobname.$i -w "done($jobname.$prev)" $cmd
 done
 exit 0
