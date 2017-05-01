617848faa662e0c9a79e136741e4629c4d4e3068
Dave Larson <dlarson@genome.wustl.edu>
2017-05-01 16:27:27 -0500

    only collapse panels in x direction

--- qc/sv/PreMergeReport.R
+++ qc/sv/PreMergeReport.R
@@ -43,6 +43,6 @@ plot_10kb_size_fingerprint <- function(count_data) {
     facet_grid(Type ~ Cohort, scales="free_x", space="free_x") + 
     scale_y_discrete(breaks=c(0, 100, 300, 1000, 10000)) + 
     scale_x_discrete(breaks=NULL) +
-    theme(panel.margin=unit(0.0, "lines"), axis.text=element_text(colour="black"), strip.text.x=element_text(size=8, angle=90)) 
+    theme(panel.margin.x=unit(0.0, "lines"), axis.text=element_text(colour="black"), strip.text.x=element_text(size=8, angle=90)) 
   print(g)
 }
