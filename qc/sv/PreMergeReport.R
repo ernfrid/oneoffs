b26eb3f51595641f56dd674fb6d63595b0ba5e22
Dave Larson <dlarson@genome.wustl.edu>
2017-05-01 15:55:08 -0500

    add het-hom ratio plot

--- qc/sv/PreMergeReport.R
+++ qc/sv/PreMergeReport.R
@@ -30,6 +30,14 @@ plot_by_sample_counts_by_type <- function(count_data, title_string="Number of SV
   print(g)
 }
 
+plot_by_sample_hethom_by_type <- function(count_data, title_string="Number of SVs per sample by type") {
+  # set colors
+  col.list=c("#e41a1c", "#377eb8", "#4daf4a", "#999999")
+  
+  g <- ggplot(data=count_data, aes(x=Sample, y=HetHomRatio, fill=Type)) + facet_grid(Type ~ Cohort, scales="free", space="free_x") + geom_bar(stat="identity", width=1, colour = "black", size=0.1) + scale_fill_manual(values = col.list) + theme(panel.margin=unit(0.0, "lines"), axis.text=element_text(colour="black"), axis.text.x=element_blank(),  axis.ticks.x=element_blank(), panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank(), strip.text.x=element_text(size=10, angle=90)) + labs(x = "Sample", y = "Het:Hom Ratio") + ggtitle(title_string) + scale_x_discrete(expand = c(0.01,0)) + guides(fill = guide_legend(override.aes = list(colour = NULL)))
+  print(g)
+}
+
 plot_10kb_size_fingerprint <- function(count_data) {
   g <- ggplot(count_data, aes(Sample, Length, fill=log10(Count))) + geom_raster() + 
     facet_grid(Type ~ ., scales="free_x", space="free_x") + 
