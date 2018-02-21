cd76d980bab5ae12b610fd0ed70c2d8d1d0b8e15
Dave Larson <dlarson@genome.wustl.edu>
2018-02-21 12:31:55 -0600

    Add pipeline parsing

--- make_flagstat_table.pl
+++ make_flagstat_table.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use File::Basename;
 use IO::File;
 use Switch;
 
@@ -18,6 +19,7 @@ while(<>) {
     chomp;
     my @fields = split /\./;
     my $type = filename_to_type($fields[-1]);
+    my ($sample, $pipeline) = filename_to_sample_pipeline($_);
     my $fh = IO::File->new($_) or die "Unable to read $_\n";
     my $dups;
     my $total;
@@ -25,15 +27,15 @@ while(<>) {
         chomp $line;
         my @fields2 = split " ", $line;
         if ($fields2[3] eq 'in') {
-            $total = $fields2[0];
-            print join("\t", "${type}_reads", $fields2[0]), "\n";
+            $total = $fields2[0] + $fields2[2];
+            print join("\t", $sample, $pipeline, "${type}_reads", $fields2[0] + $fields2[2]), "\n";
         }
         elsif ($fields2[3] eq "duplicates") {
-            $dups = $fields2[0];
-            print join("\t", "${type}_duplicates", $fields2[0]), "\n";
+            $dups = $fields2[0] + $fields2[2];
+            print join("\t", $sample, $pipeline, "${type}_duplicates", $fields2[0] + $fields2[2]), "\n";
         }
     }
-    print join("\t", "${type}_rate", $dups/$total), "\n";
+    print join("\t", $sample, $pipeline, "${type}_rate", $dups/$total), "\n";
 }
 
 sub filename_to_type {
@@ -46,4 +48,14 @@ sub filename_to_type {
         case /flagstat_supp_both_mapped/ { return 'supplementary_both_mapped'; }
         case /flagstat_supp/ { return 'supplementary'; }
     }
+}
+
+sub filename_to_sample_pipeline {
+    my ($name) = @_;
+    my $filename = fileparse($name, qw( .cram.flagstat .cram.flagstat_both_mapped_non_supp .cram.flagstat_orphan_mapped_end_non_supp .cram.flagstat_orphan_unmapped_end_non_supp .cram.flagstat_supp_both_mapped .cram.flagstat_supp ));
+    my ($sample, $pipeline, $rep) = split '\.', $filename;
+    if ($rep) {
+        $pipeline .= $rep;
+    }
+    return ($sample, $pipeline);
 }
